77d12a5b982d5359b8bb1df9710906b555193141
yuvarajbryan <yuvaraj31v@gmail.com>
2021-06-25 20:51:50 +0530

    added images,updated background

--- login.js
+++ login.js
@@ -1,6 +1,7 @@
 function myFunction() {
     var user = document.getElementById("Uname").value
     var pass = document.getElementById("Pass").value
+    console.log(user, pass)
     if (user == "admin" && pass == "admin") {
         alert("Login Successful");
     }
